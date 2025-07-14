b91c9bec88ef082e5d33fa9e1d55942098d0bb67
Will <wgledhill197@gmail.com>
2025-07-14 08:29:55 +0100

    changed nav

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -55,7 +55,7 @@ export const Navbar = () => {
   return (
     <>
       <nav className="w-full border-b border-gray-200 bg-white sticky top-0 z-50">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 py-4 flex items-center justify-between">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 py-2 md:py-4 flex items-center justify-between">
           {/* Left Links */}
           <div className="hidden md:flex gap-6">{navItems.map(renderLink)}</div>
 
